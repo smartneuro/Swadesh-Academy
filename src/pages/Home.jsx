6d1a2f26ce80bb74972d312bcf02f35df8ee07f0
anchal-github123 <anchalnayak2003@gmail.com>
2025-11-01 09:49:57 +0530

    Revert "Done"
    This reverts commit 8a5992c20febc7c9a1384378652fed2f22bb34e0.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,16 +17,16 @@ export default function Hero() {
 
       <Suspense fallback={<div>Loading....</div>}>
         <Courses />
-        {/* <Cirtificate/> */}
+        <Cirtificate/>
         
       </Suspense>
 
       {/* ✅ WhatsApp Floating Icon */}
       <a
-        href="https://wa.me/919861371576" // replace with your WhatsApp number
+        href="https://wa.me/917735755673" // replace with your WhatsApp number
         target="_blank"
         rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 animate-bounce"
+className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 animate-bounce"
       >
         <Phone size={28} />
       </a>
